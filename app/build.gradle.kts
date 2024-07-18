1cbe9a4f0521b40e915efac07741e7d4066e230b
Artur Wilczek <artwil@artwil.tech>
2024-07-18 13:24:08 +0200

    clean code.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,7 +80,6 @@ dependencies {
     testImplementation(libs.junit)
     testImplementation (libs.junit)
     testImplementation (libs.mockk)
-    testImplementation (libs.robolectric)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
